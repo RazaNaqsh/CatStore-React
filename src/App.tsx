5dd49deef980460b19d9b0588d96b164ea405e22
RazaNaqsh <owaisraza1704@gmail.com>
2023-04-01 11:51:29 +0530

    Create Folder structure and setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./pages/HomePage/HomePage";
+import Store from "./pages/Store/Store";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={<HomePage />}
+				/>
+				<Route
+					path="/store"
+					element={<Store />}
+				/>
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
