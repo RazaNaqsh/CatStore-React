cbb3ff2384eac855cd126b70eef2c7199e1a4500
RazaNaqsh <owaisraza1704@gmail.com>
2023-04-01 12:28:36 +0530

    Navbar layout

---
+++ src/components/NavButton/NavButton.tsx
@@ -0,0 +1,6 @@
+const NavButton = (props) => {
+	const { btnName, btnIcon, handleClick } = props;
+	return <button className="">{btnName}</button>;
+};
+
+export default NavButton;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,17 @@
+import NavButton from "../NavButton/NavButton";
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className="flex justify-between py-3 px-8">
+			<div>
+				<NavButton btnName="Game Store" />
+			</div>
+			<div className="space-x-10">
+				<NavButton btnName="Raza" />
+				<NavButton btnName="Cart" />
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
