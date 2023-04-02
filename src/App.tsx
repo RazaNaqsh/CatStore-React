a47e2c0123aaf61328db1280d18a19a94cd1ee72
RazaNaqsh <owaisraza1704@gmail.com>
2023-04-02 16:12:43 +0530

    GameStore turned to catStore

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,19 @@ import HomePage from "./pages/HomePage/HomePage";
 import Store from "./pages/Store/Store";
 import { useState } from "react";
 
-import catBg from "./assets/bg/catLookingAtButterfly.mp4";
+// import catBg from "./assets/bg/catLookingAtButterfly.mp4";
+import cat from "./assets/bg/catFace.jpg";
 import "./App.scss";
 import Cart from "./components/Cart/Cart";
 
 const App = () => {
-	const [cartWindow, setCartWindow] = useState(true);
+	const [cartWindow, setCartWindow] = useState(false);
 	const toggleCart = () => {
 		setCartWindow((prev) => !prev);
 	};
 	return (
 		<div>
-			<video
+			{/* <video
 				className="videoBg"
 				autoPlay
 				loop
@@ -25,7 +26,12 @@ const App = () => {
 					src={catBg}
 					type="video/mp4"
 				/>
-			</video>
+			</video> */}
+			<img
+				src={cat}
+				alt=""
+				className="screenBg"
+			/>
 			<Navbar handleCartClick={toggleCart} />
 			{cartWindow && <Cart handleCartClick={toggleCart} />}
 			<Routes>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,16 @@
+import { Link } from "react-router-dom";
 import CustomButton from "../CustomButton/CustomButton";
 
 const Navbar = ({ handleCartClick }) => {
-	const cardToggle = () => {
-		// toggle cartWindow
-	};
 	return (
 		<nav className="flex justify-between py-3 px-8 z-10ZZz">
-			<div>
-				<CustomButton btnName="Game Store" />
-			</div>
-			<div className="space-x-10">
-				<CustomButton
-					btnName="Raza"
-					handleClick={{}}
-				/>
+			<Link to="/">
+				<h3>Catt Store</h3>
+			</Link>
+			<div className="flex space-x-10">
+				<Link to="https://github.com/RazaNaqsh">
+					<h3>Raza</h3>
+				</Link>
 				<CustomButton
 					btnName="Cart"
 					handleClick={handleCartClick}
