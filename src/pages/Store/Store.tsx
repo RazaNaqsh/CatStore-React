77209d3ce3a8bc7c130bd7ac3090058d970c8766
RazaNaqsh <owaisraza1704@gmail.com>
2023-04-03 15:59:02 +0530

    Initial layout for cat section

--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -1,13 +1,18 @@
 import "./Store.scss";
 import catsData from "../../utils/catsData.js";
+import CustomButton from "../../components/CustomButton/CustomButton";
 const Store = () => {
 	const cats = catsData.map((cat) => (
-		<div>
+		<div className="relative w-fit">
 			<h1>{cat.name}</h1>
 			<img
 				src={cat.src}
 				alt="cat"
 				className="h-[250px] w-[400px] object-cover"
+			/>
+			<CustomButton
+				btnName="Buy"
+				customStyle="absolute bottom-5 left-0 right-0 bg-red-500 hover:bg-red-600"
 			/>
 		</div>
 	));
@@ -15,7 +20,9 @@ const Store = () => {
 		<main>
 			<div className="p-4">
 				<h1>Browse Your Cats here</h1>
-				<section className="p-8">{cats}</section>
+				<section className="p-8">
+					<div className="flex flex-wrap justify-evenly">{cats}</div>
+				</section>
 			</div>
 		</main>
 	);
