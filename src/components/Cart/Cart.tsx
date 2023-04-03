fa50fe737f762efccf98a4bcb998db91e38e5cf8
RazaNaqsh <owaisraza1704@gmail.com>
2023-04-03 15:57:54 +0530

    Increase z-index for cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -2,7 +2,7 @@ import CustomButton from "../CustomButton/CustomButton";
 
 const Cart = ({ handleCartClick }) => {
 	return (
-		<aside className="absolute right-0 top-0 bg-[#1A1A1A] h-full w-[25vw] min-w-[250px] py-5 px-4">
+		<aside className="absolute right-0 top-0 bg-[#1A1A1A] h-full w-[25vw] min-w-[250px] py-5 px-4 z-50">
 			<div className="flex flex-col h-full justify-between">
 				<div className="flex justify-between">
 					<h1 className="text-xl lg:text-3xl font-extrabold">No cats added</h1>
